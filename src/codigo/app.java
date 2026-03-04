35f047b5e079bd32a8a4127600f41db33ad7dbdd
Cristina <cristinald42@educastur.es>
2026-03-04 20:01:13 +0100

    Primer commit

---
+++ src/codigo/app.java
@@ -0,0 +1,11 @@
+package codigo;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Este archivo lanzará una aplicación");
+	}
+
+}
